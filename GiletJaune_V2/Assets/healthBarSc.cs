4104a62671e59344a823bb5311f8dc9b658d8496
aymericCOUPRIE <aymeric.couprie@gmail.com>
2019-02-27 16:40:11 +0100

    HealthBar début

---
+++ GiletJaune_V2/Assets/healthBarSc.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthBarSc : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float currentHealth = 100;
+    public float degatMonstre = 0.5f;
+    private float originalScale;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        originalScale = gameObject.transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tmpScale = gameObject.transform.localScale;
+        if (currentHealth >= 0)
+        {
+            currentHealth -= degatMonstre;
+            tmpScale.x = currentHealth / maxHealth * originalScale;
+        }
+        gameObject.transform.localScale = tmpScale;
+    }
+}
